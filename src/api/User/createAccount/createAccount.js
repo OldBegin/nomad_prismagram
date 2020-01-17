88eb055e56f8307ac02fa3f1ae3312cbb9adeab6
youngunyou <ygyou@me.com>
2020-01-17 17:34:16 +0800

    #3.1 createAccount 완성

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,8 +3,8 @@ import { prisma } from "../../../../generated/prisma-client";
 export default{
     Mutation:{
         createAccount: async (_,args)=>{
-            const { userName, email, firstName, lastName, bio } = args;
-            const user = await prisma.createUser({userName, email, firstName = "", lastName = "", bio = ""});
+            const { userName, email, firstName = "", lastName = "", bio = "" } = args;
+            const user = await prisma.createUser({userName, email, firstName, lastName, bio});
             return user;
         }
     }
