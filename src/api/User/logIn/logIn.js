d0f8ffd9b78080b1d1701fec9b4e8bffcb5ebbd8
unknown <ygyou@me.com>
2020-02-24 23:52:09 +0800

    API수정:login 리턴값 null 반환가능하도록 함-에러처리 프론트에서 하기위함

--- src/api/User/logIn/logIn.js
+++ src/api/User/logIn/logIn.js
@@ -10,12 +10,16 @@ export default {
 
             const user = await prisma.user({email});
             if(!user){
-                throw new Error(' :::[API]logIn::: No such user found:::');
+                console.log(' :::[API]logIn::: No such user found:::')
+                return;
+                //throw new Error(' :::[API]logIn::: No such user found:::');
             }
 
             const decodedPwd = await bcryptjs.compare(password,user.password)
             if(!decodedPwd){
-                throw new Error(' :::[API]logIn::: Your combination is incorrect::: ');
+                console.log(' :::[API]logIn::: Your combination is incorrect:::')
+                return;
+                //throw new Error(' :::[API]logIn::: Your combination is incorrect::: ');
             }
             const token = generateToken(user.id, user.email, process.env.SECRET, '60m'); // unpacked token: email
             console.log(` :::[API]logIn::: Success issueing token to user: ${email} :::`);
